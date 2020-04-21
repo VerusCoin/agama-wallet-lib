1e5973b29564e6df5b0f3437a6eb48538b61023f
Michael Toutonghi <toutonghi.michael@gmail.com>
2020-04-21 14:05:22 +0200

    Add lightwalletd servers

---
+++ src/dlight-servers.js
@@ -0,0 +1,9 @@
+let dlightServers = {
+  vrsc: ['light.virtualsoundnw.com:9077'],
+  zec: ['lightwalletd.z.cash:9067'],
+  zectest: ['lightwalletd.testnet.z.cash:9067']
+};
+
+module.exports = {
+  dlightServers
+};
